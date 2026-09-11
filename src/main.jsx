c2e83b52ed21cf28066f06fbf3dd07188a683fc4
barbarasouzza <barbarasoouzza@gmail.com>
2026-09-11 11:04:11 -0300

    fix: corrige caminhos das imagens no GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/festival-gabiroba">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
